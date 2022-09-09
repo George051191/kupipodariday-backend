2095d60849c9b52fee262c0a44b2f83b916aa24f
George051191 <trubacheff_91@mail.ru>
2022-09-09 08:53:40 +0300

    get methods fo user added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import bcrypt from 'bcryptjs';
+import { JwtGuard } from 'src/guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
@@ -35,10 +38,27 @@ export class UsersController {
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
+  @UseGuards(JwtGuard)
+  @Get('me')
+  findUser(@Req() req) {
+    return this.usersService.find({ username: req.user.username });
+  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @UseGuards(JwtGuard)
+  @Get(':username')
+  findCurrentUser(@Param('username') username: string) {
+    return this.usersService.find({ username: username });
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch('me')
+  update(@Req() req, @Body() updateUserDto: UpdateUserDto) {
+    const { password, ...res } = updateUserDto;
+    return bcrypt
+      .hash(password, 10)
+      .then((hash: string) =>
+        this.usersService.update(req.user.id, { password: hash, ...res }),
+      );
   }
 
   @Delete(':id')
