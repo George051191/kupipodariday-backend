a616b487847d0479e28f82e5651eb82af161e384
George051191 <trubacheff_91@mail.ru>
2022-09-19 09:31:02 +0300

    fixes done

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,6 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { WishesModule } from 'src/wishes/wishes.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
